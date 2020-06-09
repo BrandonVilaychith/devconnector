5bad4cf5d4a763fb9ef7658e09a072d8ef6d5116
Brandon Vilaychith <brandonlvilaychith@gmail.com>
2020-06-09 00:16:35 -0500

    Init

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectBD = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectBD();
+
+// Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Runing"));
+
+// Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
